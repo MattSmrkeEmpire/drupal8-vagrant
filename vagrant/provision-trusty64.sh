d7d6881f497bd346f7c07d459728cd5f95b06078
Matt Smrke <matthew.smrke@empire.ca>
2016-02-03 19:12:21 -0500

    adds drush and ssh key

--- vagrant/provision-trusty64.sh
+++ vagrant/provision-trusty64.sh
@@ -3,43 +3,49 @@
 # Intended for Ubuntu 14.04 (Trusty)
 
 # Adjust timezone to be Toronto
-ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
+sudo ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
 
 
 # Install Apache, PHP
 echo "Updating repositories"
-apt-get install software-properties-common python-software-properties build-essential -y > /dev/null
-add-apt-repository ppa:ondrej/php5 -y > /dev/null
-apt-get update > /dev/null
+sudo apt-get install software-properties-common python-software-properties build-essential -y > /dev/null
+sudo add-apt-repository ppa:ondrej/php5 -y > /dev/null
+sudo apt-get update > /dev/null
 
 echo "----- Installing Apache, PHP and MySQL"
-apt-get install apache2 php5 libapache2-mod-php5 -y > /dev/null
-echo "ServerName localhost" > "/etc/apache2/conf-available/fqdn.conf"
-a2enconf fqdn > /dev/null
-a2enmod rewrite > /dev/null
-a2dissite 000-default.conf > /dev/null
+sudo apt-get install apache2 php5 libapache2-mod-php5 -y > /dev/null
+sudo echo "ServerName localhost" > "/etc/apache2/conf-available/fqdn.conf"
+sudo a2enconf fqdn > /dev/null
+sudo a2enmod rewrite > /dev/null
+sudo a2dissite 000-default.conf > /dev/null
 
 
 echo "Installing PHP extensions and utils"
-apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y  > /dev/null
-apt-get install debconf-utils -y > /dev/null
+sudo apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y  > /dev/null
+sudo apt-get install debconf-utils -y > /dev/null
 
 
 # Cleanup
-apt-get -y autoremove > /dev/null
+sudo apt-get -y autoremove > /dev/null
 
 # Set up Apache
 echo "----- Finishing Apache setup ..."
-rm -rf /var/www/html
-rm /etc/apache2/envvars
-cp /vagrant/apache-envvars /etc/apache2/envvars
-service apache2 restart > /dev/null
+sudo rm -rf /var/www/html
+sudo rm /etc/apache2/envvars
+sudo cp /vagrant/apache-envvars /etc/apache2/envvars
+sudo service apache2 restart > /dev/null
+
+# Composer
+echo "---- Installing Composer and Drush ..."
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo composer global require drush/drush
+sudo ln -s ~/.composer/vendor/drush/drush/drush /usr/local/bin/drush
 
 # Drupal Console
 echo "----- Installing Drupal Console..."
 curl https://drupalconsole.com/installer -L -o drupal.phar
-mv drupal.phar /usr/local/bin/drupal
-chmod +x /usr/local/bin/drupal
+sudo mv drupal.phar /usr/local/bin/drupal
+sudo chmod +x /usr/local/bin/drupal
 drupal init --override
 
 
